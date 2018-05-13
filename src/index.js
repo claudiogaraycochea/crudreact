f53c97c020558033e2c6458b4e7e283aebe68b95
Claudio Garaycochea <computadoraweb@gmail.com>
2018-05-13 19:40:59 -0300

    Project Upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import store from './store';
+import { Provider } from 'react-redux';
+import { loadProducts } from './actions';
+
+store.dispatch(loadProducts());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();
---
+++ src/page404/page404.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Page404 extends Component {
+  render (){
+    return (<div>
+      Error404
+    </div>);
+  };    
+}
+
+export default Page404
